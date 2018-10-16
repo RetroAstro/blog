387513c7d612a421cf12684eae9fe974cbc729e0
RetroAstro <452461114@qq.com>
2018-10-16 23:44:52 +0800

    async

--- space/11/index.js
+++ space/11/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
-const addAll = () => console.log(countArray.reduce((prev, cur) => prev + cur))
+const addAll = (result) => console.log(result.reduce((prev, cur) => prev + cur))
 
 let dir = path.join(__dirname, 'files')
 
+// callback
 let taskLength, taskCount = 0, countArray = []
 
 fs.readdir(dir, (err, files) => {
@@ -18,7 +19,24 @@ fs.readdir(dir, (err, files) => {
             data.split(' ').map(word => word === 'of' ? count++ : null)
             countArray.push(count)
             taskCount++
-            taskCount === taskLength ? addAll() : null
+            taskCount === taskLength ? addAll(countArray) : null
         })
     })
+})
+
+// promise 
+fs.readdir(dir, (err, files) => {
+    if (err) return console.error(err)
+    let promises = files.map((file) => {
+        return new Promise((resolve, reject) => {
+            let fileDir = path.join(dir, file)
+            fs.readFile(fileDir, { encoding: 'utf-8' }, (err, data) => {
+                if (err) reject(err)
+                let count = 0
+                data.split(' ').map(word => word === 'of' ? count++ : null)
+                resolve(count)
+            })
+        })
+    })
+    Promise.all(promises).then(result => addAll(result))
 })
