807726c2956de40876eba6eb86214aa618de7de3
RetroAstro <452461114@qq.com>
2018-10-17 12:25:33 +0800

    fix

--- space/11/index.js
+++ space/11/index.js
@@ -5,28 +5,9 @@ const addAll = (result) => console.log(result.reduce((prev, cur) => prev + cur))
 
 let dir = path.join(__dirname, 'files')
 
-// callback
-let taskLength, taskCount = 0, countArray = []
-
 fs.readdir(dir, (err, files) => {
     if (err) return console.error(err)
-    taskLength = files.length
-    files.map((file) => {
-        let fileDir = path.join(dir, file)
-        fs.readFile(fileDir, { encoding: 'utf-8' }, (err, data) => {
-            if (err) return console.error(err)
-            let count = 0
-            data.split(' ').map(word => word === 'of' ? count++ : null)
-            countArray.push(count)
-            taskCount++
-            taskCount === taskLength ? addAll(countArray) : null
-        })
-    })
-})
 
-// promise 
-fs.readdir(dir, (err, files) => {
-    if (err) return console.error(err)
     let promises = files.map((file) => {
         return new Promise((resolve, reject) => {
             let fileDir = path.join(dir, file)
@@ -38,5 +19,7 @@ fs.readdir(dir, (err, files) => {
             })
         })
     })
-    Promise.all(promises).then(result => addAll(result))
+    
+    Promise.all(promises).then(result => addAll(result)).catch(err => console.error(err))
+    Promise.race(promises).then(result => console.log(result)).catch(err => console.error(err))
 })
