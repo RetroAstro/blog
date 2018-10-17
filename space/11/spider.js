644afba1d3f83cbb58be886ba91bd6d69d3a4e34
RetroAstro <452461114@qq.com>
2018-10-18 00:26:37 +0800

    update spider.js

--- space/11/spider.js
+++ space/11/spider.js
@@ -3,7 +3,39 @@ const path = require('path')
 const request = require('request')
 const cheerio = require('cheerio')
 
+const target = `http://www.zimuxia.cn/${encodeURIComponent('我们的作品')}`
+
 const isError = (err, res) => (err || res.statusCode !== 200) ? true : false
+
+const getImgUrls = function (pages) {
+    return new Promise((resolve) => {
+        let limit = 5, number = 0, imgUrls = []
+        const recursive = async function () {
+            pages = pages - limit
+            limit = pages >= 0 ? limit : (pages + limit)
+            let arr = []
+            for (let i = 1; i <=limit; i++) {
+                arr.push(
+                    new Promise((resolve) => {
+                        request(target + `?set=${number++}`, (err, res, data) => {
+                            if (isError(err, res)) return console.log('Request failed.')
+                            let $ = cheerio.load(data)
+                            $('.pg-page-wrapper img').each((i, el) => {
+                                let imgUrl = $(el).attr('data-cfsrc')
+                                imgUrls.push(imgUrl)
+                                resolve()
+                            })
+                        })
+                    })
+                )
+            }
+            await Promise.all(arr)
+            if (limit === 5) return recursive()
+            resolve(imgUrls)
+        }
+        recursive()
+    })
+}
 
 const downloadImages = function (arr) {
     let promises = arr.map((url) => {
@@ -18,19 +50,16 @@ const downloadImages = function (arr) {
     Promise.all(promises).then(() => console.log('Download finished.'))
 }
 
-let page = 1
-
 request({
-    url: `http://www.zimuxia.cn/${encodeURIComponent('我们的作品')}?set=${page}`,
+    url: target,
     method: 'GET'
 }, (err, res, data) => {
     if (isError(err, res)) return console.log('Request failed.')
     let $ = cheerio.load(data)
-    let arr = []
-    $('.pg-page-wrapper img').each((i, el) => {
-        let imgUrl = $(el).attr('data-cfsrc')
-        arr.push(imgUrl)
+    let pageNum = $('.pg-pagination li').length
+    getImgUrls(pageNum)
+    .then((result) => {
+        // downloadImages(result)
     })
-    downloadImages(arr)
 })
 
